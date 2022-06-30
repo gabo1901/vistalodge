0319df178b561753c357e6c6e3012f0e950ffa01
gabo1901 <gabrielvega888@gmail.com>
2022-06-30 12:54:56 -0600

    cfvghbjnkm

--- js/index.js
+++ js/index.js
@@ -58,10 +58,91 @@ function scrollHorizontaly(val) {
         var alturaAnimado = animado[i].offsetTop;
 
          if(alturaAnimado -400 < scrollTop){
-             animado[i].style.opacity = "1";
+             animado[i].style.opacity = "1"; 
              animado[i].classList.add("animados")
 
          }
      }
  }
 window.addEventListener("scroll", mostrarScroll);
+
+
+
+
+const btnCierra = document.querySelector(".btn-cierra");
+const imagenes = document.querySelectorAll(".habita1 img");
+const lightbox = document.querySelector(".contenedor-principal");
+const imagenActiva = document.querySelector(".imagen-activa");
+var indiceImagen = 0;
+
+// Abre LightHouse
+
+const abreLightBox = (e) =>{
+    imagenActiva.src = e.target.src;
+    lightbox.style.display = "flex"
+    indiceImagen = Array.from(imagenes).indexOf(e.target);
+}
+
+imagenes.forEach((imagen)=>{
+    imagen.addEventListener('click', abreLightBox);
+});
+
+// Cierre LightHouse
+
+btnCierra.addEventListener('click', ()=>{
+    lightbox.style.display = "none";
+})
+
+
+
+// **************************** Habitación Dos *************************
+
+const btnCierra2 = document.querySelector(".btn2-cierra");
+const imagenes2 = document.querySelectorAll(".habita2 img");
+const lightbox2 = document.querySelector(".contenedor-principal2");
+const imagenActiva2 = document.querySelector(".imagen-activa2");
+var indiceImagen2 = 0;
+
+// Abre LightHouse
+
+const abreLightBox2 = (e) =>{
+    imagenActiva2.src = e.target.src;
+    lightbox2.style.display = "flex"
+    indiceImagen2 = Array.from(imagenes2).indexOf(e.target);
+}
+
+imagenes2.forEach((imagen)=>{
+    imagen.addEventListener('click', abreLightBox2);
+});
+
+// Cierre LightHouse
+
+btnCierra2.addEventListener('click', ()=>{
+    lightbox2.style.display = "none";
+})
+
+// **************************** Habitación Tres *************************
+
+const btnCierra3 = document.querySelector(".btn3-cierra");
+const imagenes3 = document.querySelectorAll(".habita3 img");
+const lightbox3 = document.querySelector(".contenedor-principal3");
+const imagenActiva3 = document.querySelector(".imagen-activa3");
+var indiceImagen3 = 0;
+
+// Abre LightHouse
+
+const abreLightBox3 = (e) =>{
+    imagenActiva3.src = e.target.src;
+    lightbox3.style.display = "flex"
+    indiceImagen3 = Array.from(imagenes3).indexOf(e.target);
+}
+
+imagenes3.forEach((imagen)=>{
+    imagen.addEventListener('click', abreLightBox3);
+});
+
+// Cierre LightHouse
+
+btnCierra3.addEventListener('click', ()=>{
+    lightbox3.style.display = "none";
+})
